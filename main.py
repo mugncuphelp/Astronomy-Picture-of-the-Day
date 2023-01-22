1a7577e7b46eda24d6e7d40d7bc00d51059012be
mugncuphelp <mugncuphelp@gmail.com>
2023-01-22 22:09:36 +0800

    build a streamlit webapp to display astronomy photo from NASA api

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import streamlit as st
+import requests
 
 api = 'Kz9Cd6ANCrAqkQlmoctNMq7Z9ZaZPEJGdJxdmhMv'
 
 url = f'https://api.nasa.gov/planetary/apod?api_key={api}'
 
+
+response = requests.get(url)
+content = response.json()
+
+# download the image
+image_filepath = 'image.png'
+image_response = requests.get(content['url'])
+with open (image_filepath, 'wb') as file:
+    file.write(image_response.content)
+
+st.title(content["title"])
+st.image(content['url'])
+st.write(content['explanation'])
 
 
